41d3833205125e0031697f50d28ea19f626a9179
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-01-08 22:10:12 +0900

    customView UIKit 공부

---
+++ CustomView.uikit/FocusView.swift
@@ -0,0 +1,20 @@
+//
+//  FocusView.swift
+//  CustomView.uikit
+//
+//  Created by 이윤지 on 1/8/24.
+//
+
+import UIKit
+
+class FocusView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
