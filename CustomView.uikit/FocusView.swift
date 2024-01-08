1ad590ce0e5dbecd0f3dbb6ffe88891ea37b7d6e
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-01-08 22:10:34 +0900

    customView UIKit 공부

--- CustomView.uikit/FocusView.swift
+++ CustomView.uikit/FocusView.swift
@@ -9,12 +9,22 @@ import UIKit
 
 class FocusView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override var canBecomeFocused: Bool {
+           return true
+       }
+    
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        
+        if context.previouslyFocusedView === self {
+            UIView.animate(withDuration: 0.1, animations: { () -> Void in
+                context.previouslyFocusedView?.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            })
+        }
+        
+        if context.nextFocusedView === self {
+            UIView.animate(withDuration: 0.1, animations: { () -> Void in
+                context.nextFocusedView?.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            })
+        }
     }
-    */
-
 }
